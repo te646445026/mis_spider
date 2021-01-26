ff30d4137c24256bd8fef47577a8042f59861abc
cjh <cjh646445026@gmail.com>
2021-01-26 23:44:07 +0800

    test

---
+++ mis_spider.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+import time
+
+username = 'jmyxh'
+password = 'jmyxh007'
+
+browser = webdriver.Ie()
+
+browser.get('http://mis.gdsei.org.cn/')
+
+time.sleep(5)
+
+#找到用户框，输入账号
+inputName = browser.find_element_by_css_selector('input#j_username')
+inputName.send_keys(username)
+
+#找到密码框，输入密码
+inputPassword = browser.find_element_by_css_selector('input#j_password')
+inputPassword.send_keys(password)
+
+#找到登录按钮，点击
+button  = browser.find_element_by_css_selector('a#loginButton')
+button.click()
+
+time.sleep(5)
